a9e6e338eaee21856700806a0c6994692493489b
Sebastian Spier <github@spier.hu>
2021-01-23 11:34:22 +0100

    InnerSource crawler implementation incl. _InnerSourceMetadata

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # innersource-crawler-ruby
-This project creates a repos.json that can be utilized by the SAP InnerSource Portal.
+
+This project creates a `repos.json` that can be utilized by the [SAP InnerSource Portal][SAP-InnerSource-Portal]. The current approach assumes that the repos that you want to show in the portal are available in a GitHub organization, and that they all are tagged with a certain _topic_.
+
+## Installation
+
+`bundle install`
+
+## Usage
+
+1. Copy `.env-example` to `.env`
+1. Fill out the `.env` file with a _token_ from a user that has access to the organization to scan (listed below)
+1. Fill out the `.env` file with the exact _topic_ name you are searching for
+1. Fill out the `.env` file with the exact _organization_ that you want to search in
+1. Run `ruby ./crawler.rb`, which will create a `repos.json` file containing the relevant metadata for the GitHub repos for the given _topic_
+1. Copy `repos.json` to your instance of the [SAP-InnerSource-Portal][SAP-InnerSource-Portal] and launch the portal as outlined in their installation instructions
+
+[SAP-InnerSource-Portal]: https://github.com/sap/project-portal-for-InnerSource
---
+++ crawler.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+Dotenv.load
+
+# A dummy implementation of how to calculate an activity score for a repository.
+# The activity score can be used for sorting the repos in the portal.
+# For inspiration of how to calculate such a score see https://innersourcecommons.gitbook.io/innersource-patterns/patterns/repository-activity-score
+def calculateScore(repo)
+  (1 + repo.stargazers_count) * (1 + repo.watchers_count) - repo.open_issues
+end
+
+# tests whether the file with `filename` exists in the GitHub `repo`
+def fileExists?(client, repo, filename)
+  fileExists = true
+  begin
+    client.contents(repo.full_name, :path => filename)
+  rescue Octokit::NotFound => ex
+    fileExists = false
+  end
+  return fileExists
+end
+
+# -------------------
+# Main block
+# -------------------
+
+# Auth to GitHub.com
+client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
+client.auto_paginate = true
+
+# Get repos for the configured ORGANIZATION and TOPIC
+# Note: The GitHub Search API limits search results to max 1.000 repos.
+search_string = "org:#{ENV['ORGANIZATION']} topic:#{ENV['TOPIC']}"
+repos = client.search_repos(search_string)
+
+repos_output = []
+
+repos[:items].each do |repo|
+  puts repo.full_name
+
+  participation_stats = client.participation_stats(repo.full_name)
+
+  repo["_InnerSourceMetadata"] = {}
+  repo["_InnerSourceMetadata"]["participation"] = participation_stats["all"]
+  repo["_InnerSourceMetadata"]["score"] = calculateScore(repo)
+
+  if (fileExists?(client, repo, "CONTRIBUTING.md"))
+    repo["_InnerSourceMetadata"]["guidelines"] = "CONTRIBUTING.md"
+  else
+    repo["_InnerSourceMetadata"]["guidelines"] = nil
+  end
+
+  # Note: We are serializing the full repository metadata here, even though the SAP InnerSource Portal only needs a subset of those attributes. One could save some storage space here and increase readibility of the `repos.json` file.
+  repos_output << repo.to_hash
+end
+
+File.write('repos.json', JSON.pretty_generate(repos_output))

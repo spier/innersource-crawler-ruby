7c117269e93c590faa52c4cb2016c9de7f7aaa05
Sebastian Spier <github@spier.hu>
2021-07-10 07:22:59 +0200

    Crawl topics and place them into the correct area of the JSON structure (i.e. below _InnerSourceMetadata).

--- crawler.rb
+++ crawler.rb
@@ -28,7 +28,7 @@ end
 # Auth to GitHub.com
 client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
 client.auto_paginate = true
-client.default_media_type = "application/vnd.github.v3+json"
+client.default_media_type = "application/vnd.github.mercy-preview+json"
 
 # Get repos for the configured ORGANIZATION and TOPIC
 # Note: The GitHub Search API limits search results to max 1.000 repos.
@@ -45,6 +45,10 @@ repos[:items].each do |repo|
   repo["_InnerSourceMetadata"] = {}
   repo["_InnerSourceMetadata"]["participation"] = participation_stats["all"]
   repo["_InnerSourceMetadata"]["score"] = calculateScore(repo)
+
+  # move topics from root into _InnerSourceMetadata
+  repo["_InnerSourceMetadata"]["topics"] = repo.topics
+  repo.delete("topics")
 
   if (fileExists?(client, repo, "CONTRIBUTING.md"))
     repo["_InnerSourceMetadata"]["guidelines"] = "CONTRIBUTING.md"
